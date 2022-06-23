6a0334b724b55f6e718ec197697deaea00cd6ef3
tomkimsour <tomkimsour@hotmail.fr>
2022-06-23 13:31:56 +0930

    start standard animation

---
+++ scripts/move.py
@@ -0,0 +1,29 @@
+import qi
+import sys
+
+
+class Motion():
+    def __init__(self, app):
+        app.start()
+        session = app.session
+        animation_player_service = session.service("ALAnimationPlayer")
+
+    def animate(name:str):
+        future = animation_player_service.run(name, _async=True)
+        # wait the end of the animation
+        future.value()
+
+
+
+if __name__ == "__main__":
+    try:
+        connection_url = "tcp://127.0.0.1:9559"
+        app = qi.Application(
+            ["SoundProcessingModule", "--qi-url=" + connection_url])
+    except RuntimeError:
+        print("Can't connect to Naoqi at ip \"" + ip + "\" on port " + str(port) + ".\n"
+              "Please check your script arguments. Run with -h option for help.")
+        sys.exit(1)
+    motion = Motion(app)
+    motion.animate("animations/Stand/BodyTalk/BodyTalk_1")
+
