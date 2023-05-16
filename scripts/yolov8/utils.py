147ab429d6ead9e76f337a50549bfc3c9f077975
DoDucNhan <doducnhan1999@gmail.com>
2023-05-16 11:30:42 +0930

    change class name

--- scripts/yolov8/utils.py
+++ scripts/yolov8/utils.py
@@ -1,27 +1,29 @@
 import numpy as np
 import cv2
 
-class_names = ['002_master_chef_can',
-'003_cracker_box',
-'004_sugar_box',
-'005_tomato_soup_can',
-'006_mustard_bottle',
-'007_tuna_fish_can',
-'008_pudding_box',
-'009_gelatin_box',
-'010_potted_meat_can',
-'011_banana',
-'019_pitcher_base',
-'021_bleach_cleanser',
-'024_bowl',
-'025_mug',
-'035_power_drill',
-'036_wood_block',
-'037_scissors',
-'040_large_marker',
-'051_large_clamp',
-'052_extra_large_clamp',
-'061_foam_brick']
+# class_names = ['002_master_chef_can',
+# '003_cracker_box',
+# '004_sugar_box',
+# '005_tomato_soup_can',
+# '006_mustard_bottle',
+# '007_tuna_fish_can',
+# '008_pudding_box',
+# '009_gelatin_box',
+# '010_potted_meat_can',
+# '011_banana',
+# '019_pitcher_base',
+# '021_bleach_cleanser',
+# '024_bowl',
+# '025_mug',
+# '035_power_drill',
+# '036_wood_block',
+# '037_scissors',
+# '040_large_marker',
+# '051_large_clamp',
+# '052_extra_large_clamp',
+# '061_foam_brick']
+
+class_names = ['ball', 'green_ball', 'hammer', 'lemon', 'pringle', 'scissor', 'spoon', 'sugar_box', 'tape', 'vase']
 
 # Create a list of colors for each class where each color is a tuple of 3 integer values
 rng = np.random.default_rng(3)
