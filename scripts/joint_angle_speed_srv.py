ffc23fb7787c01f0a60126ec2d953971087e3a87
tomkimsour <tomkimsour@gmail.com>
2023-06-22 06:39:11 +0000

    yes

---
+++ scripts/joint_angle_speed_srv.py
@@ -0,0 +1,46 @@
+import qi
+import sys
+import numpy as np
+import rospy
+from robobreizh_msgs.srv import joint_position, joint_positionResponse
+
+
+class Motion():
+    def __init__(self, app):
+        app.start()
+        session = app.session
+        self.motion_service = session.service("ALMotion")
+        rospy.init_node('look_up')
+        rospy.Service('/robobreizh/manipulation/joint_angle_speed_srv', joint_position, self.animate)
+        rospy.spin()
+
+    def animate(self, req):
+        # Example showing multiple trajectories
+        names = req.joint_names
+        angle_list = []
+        for angle_list_req in req.angle_lists:
+                angle_list.append(np.deg2rad(angle_list_req.data).tolist())
+
+        time_list = []
+        for time_list_req in req.time_lists:
+                time_list.append(time_list_req.data)
+
+        angleLists = angle_list
+
+        # set times for joints every seconds
+        timeLists = req.time_lists
+        isAbsolute = True
+        self.motion_service.angleInterpolation(names, angleLists, timeLists, isAbsolute)
+        return (joint_positionResponse())
+
+
+if __name__ == "__main__":
+    try:
+        connection_url = "tcp://127.0.0.1:9559"
+        app = qi.Application(
+            ["SoundProcessingModule", "--qi-url=" + connection_url])
+    except RuntimeError:
+        print("Can't connect to Naoqi at ip \"" + ip + "\" on port " + str(port) + ".\n"
+              "Please check your script arguments. Run with -h option for help.")
+        sys.exit(1)
+    motion = Motion(app)
